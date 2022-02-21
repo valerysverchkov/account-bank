05bd9895634bf93052e4488c10472fe53ca3c8db
Сверчков Валерий Олегович (4057339) <vosverchkov@dev.vtb.ru>
2022-02-21 20:22:25 +0300

    Init commit

---
+++ src/main/java/ru/iteco/accountbank/AccountBankApplication.java
@@ -0,0 +1,13 @@
+package ru.iteco.accountbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBankApplication.class, args);
+    }
+
+}
